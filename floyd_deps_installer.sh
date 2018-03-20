21f13d1016aa8ed82521bc8092727445a1e5990f
Guillaume <jeusel.guillaume@gmail.com>
2018-03-20 19:08:28 +0100

    Update floyd files

--- floyd_deps_installer.sh
+++ floyd_deps_installer.sh
@@ -1,2 +1,10 @@
+# sym link to dataset:
+ln -s /data .
+
+# install ml_utils
 git clone https://github.com/gjeusel/ml_utils
-pip install -e ml_utils/
+cd ml_utils
+pip install -e .
+
+# Update torchvision without uninstall old one (else error):
+pip install --ignore-installed --no-dependencies torchvision==0.2.0
